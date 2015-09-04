a7fd22fbc5118aa03ca8f72de20274eedfde643b
Jason Smith (MacBook) <jason.h.smith@gmail.com>
2015-09-04 13:14:04 +0700

    Use the MARK comments since they show up in the IDE

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -9,14 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
+    // MARK: Actions
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
