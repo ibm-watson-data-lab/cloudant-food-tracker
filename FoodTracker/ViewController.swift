d493b10c05b0342ee0f5bebfec3d7bc6fc0ac16e
Jason Smith (iMac) <jason.h.smith@gmail.com>
2015-09-05 11:14:45 +0700

    Hook the rating control into the view control

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
