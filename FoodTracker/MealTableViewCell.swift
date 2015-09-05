7bec56ed88083d2abfb1c123393c57470514c155
Jason Smith (iMac) <jason.h.smith@gmail.com>
2015-09-05 13:47:30 +0700

    Prepare prototype meal table cell

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Jason Smith on 9/5/2558 BE.
+//
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
