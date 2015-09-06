20b1f32040bcf29185b0bb55958dfe41c908c41e
Jason Smith (MacBook) <jason.h.smith@gmail.com>
2015-09-06 10:48:47 +0700

    Implement NSCoding and set up the archive URL for the Meal class

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -8,12 +8,25 @@
 
 import UIKit
 
-class Meal {
+class Meal: NSObject, NSCoding {
     // MARK: Properties
     
     var name: String
     var photo: UIImage?
     var rating: Int
+    
+    // MARK: Archiving Paths
+    
+    static let DocumentsDirectory = NSFileManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.URLByAppendingPathComponent("meals")
+    
+    // MARK: Types
+    
+    struct PropertyKey {
+        static let nameKey = "name"
+        static let photoKey = "photo"
+        static let ratingKey = "rating"
+    }
     
     // MARK: Initialization
     
@@ -23,8 +36,30 @@ class Meal {
         self.photo = photo
         self.rating = rating
         
+        super.init()
+        
         if name.isEmpty || rating < 0 {
             return nil
         }
+    }
+    
+    // MARK: NSCoding
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(name, forKey: PropertyKey.nameKey)
+        aCoder.encodeObject(photo, forKey: PropertyKey.photoKey)
+        aCoder.encodeInteger(rating, forKey: PropertyKey.ratingKey)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        let name = aDecoder.decodeObjectForKey(PropertyKey.nameKey) as! String
+        
+        // Because photo is an optional property, use a conditional cast.
+        let photo = aDecoder.decodeObjectForKey(PropertyKey.photoKey) as? UIImage
+        
+        let rating = aDecoder.decodeIntegerForKey(PropertyKey.ratingKey)
+        
+        // Call the designated initializer.
+        self.init(name: name, photo: photo, rating: rating)
     }
 }
