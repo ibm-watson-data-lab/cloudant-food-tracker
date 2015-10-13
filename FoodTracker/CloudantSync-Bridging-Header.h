52f7c9b659a5d013044beea0cf780de617386f08
Jason Smith <jason.h.smith@gmail.com>
2015-10-13 13:05:56 +0700

    The bridging header

---
+++ FoodTracker/CloudantSync-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  CloudantSync-Bridging-Header.h
+//  FoodTracker
+//
+//  Created by Jason Smith on 10/13/15.
+//
+//
+
+#ifndef CloudantSync_Bridging_Header_h
+#define CloudantSync_Bridging_Header_h
+
+#import <CloudantSync.h>
+
+#endif /* CloudantSync_Bridging_Header_h */
